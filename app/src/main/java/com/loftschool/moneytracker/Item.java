1f5664ac057ff319f35dccf345d3882a838b1d9a
Ilya <emel.9616@gmail.com>
2017-06-26 22:54:10 +0700

    Add new layout - item and items. Add new java class - Item (constructor for Item) and MainActivity (keep List<Item> and responsible for the output of this array); TempActivity (former MainActivity)

---
+++ app/src/main/java/com/loftschool/moneytracker/Item.java
@@ -0,0 +1,11 @@
+package com.loftschool.moneytracker;
+
+class Item {
+    final String name;
+    final int price;
+
+    Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
