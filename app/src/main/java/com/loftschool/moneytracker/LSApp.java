c30fc52fe9477e7b93993cdaab967cc9b555afd3
Ilya <emel.9616@gmail.com>
2017-07-10 23:32:27 +0700

    Added icons

--- app/src/main/java/com/loftschool/moneytracker/LSApp.java
+++ app/src/main/java/com/loftschool/moneytracker/LSApp.java
@@ -42,6 +42,7 @@ public class LSApp extends Application {
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://loftschoolandroid.getsandbox.com/")
+                //.baseUrl("http://android.loftschool.com/basic/v1/")
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .client(client)
                 .build();
